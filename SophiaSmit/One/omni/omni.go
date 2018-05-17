98c4e982852d2ccd3fa5d2057c446f23dbd59c28
sophiasmit <sophiasmit59@gmail.com>
2018-05-17 23:51:13 +0300

    Личная проверка

--- SophiaSmit/One/omni/omni.go
+++ SophiaSmit/One/omni/omni.go
@@ -2,17 +2,17 @@ package omni
 
 func SignIn() bool {
 	var log bool
-	var pass string
-	var status bool
-	login := "Stas"
-	password := "123"
-	//error := "Неверный логин или пароль\n"
-	if log == login && pass == password {
-		status = true
-		//dateTimeEnd := 10
-	} else {
-		status = false
-	}
+	/*	var pass string
+		var status bool
+		login := "Stas"
+		password := "123"
+		//error := "Неверный логин или пароль\n"
+		if log == login && pass == password {
+			status = true
+			//dateTimeEnd := 10
+		} else {
+			status = false
+		}*/
 
 	return log
 }
