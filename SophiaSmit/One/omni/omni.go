0335988d92adf323eb7c9dec24a544abfa1483a5
sophiasmit <sophiasmit59@gmail.com>
2018-05-17 23:45:46 +0300

    Изменения SignIn

--- SophiaSmit/One/omni/omni.go
+++ SophiaSmit/One/omni/omni.go
@@ -1,18 +1,18 @@
 package omni
 
-func SignIn() string {
-	var log string
-	/*var pass string
+func SignIn() bool {
+	var log bool
+	var pass string
 	var status bool
 	login := "Stas"
 	password := "123"
 	//error := "Неверный логин или пароль\n"
-	/*if log == login && pass == password {
+	if log == login && pass == password {
 		status = true
 		//dateTimeEnd := 10
 	} else {
 		status = false
-	}*/
+	}
 
 	return log
 }
