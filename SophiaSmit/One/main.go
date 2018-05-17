c547d6010f30a6f4895bf04f09e4a2dcfa1a152c
sophiasmit <sophiasmit59@gmail.com>
2018-05-17 23:57:49 +0300

    Вывод true false в SignIn

--- SophiaSmit/One/main.go
+++ SophiaSmit/One/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"github.com/BobRocky/omnicomm/SophiaSmit/One/omni"
 )
 
-var log string
-var pass string
-var a string
-
-func Scan1() string {
-	in := bufio.NewScanner(os.Stdin)
-	in.Scan()
-	if err := in.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
-	}
-	return in.Text()
-}
+var a bool
 
 func main() {
 
--- SophiaSmit/One/omni/omni.go
+++ SophiaSmit/One/omni/omni.go
@@ -3,18 +3,18 @@ package omni
 func SignIn() string {
 	var log string
 	log = Scan1()
-	//pass = Scan1()
-	/*	var pass string
-		var status bool
-		login := "Stas"
-		password := "123"
-		//error := "Неверный логин или пароль\n"
-		if log == login && pass == password {
-			status = true
-			//dateTimeEnd := 10
-		} else {
-			status = false
-		}*/
+	pass = Scan1()
+	var pass string
+	var status bool
+	login := "Stas"
+	password := "123"
+	//error := "Неверный логин или пароль\n"
+	if log == login && pass == password {
+		status = true
+		//dateTimeEnd := 10
+	} else {
+		status = false
+	}
 
 	return log
 }
